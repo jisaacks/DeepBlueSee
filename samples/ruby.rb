a5927ee238ae8160e244167673ce2b2a0c7ea974
JD Isaacks <jd@jisaacks.com>
2013-08-28 21:36:56 -0400

    Add more ruby defs

--- samples/ruby.rb
+++ samples/ruby.rb
@@ -7,11 +7,16 @@ module MyModule
   class MyClass < Some::OtherClass
     def initialize
       hash[:symbol] =~ /myregex/i
+      
+      arr = [1,2,3]
+      hsh = {key: val, str: "str", :old => :style}
+
+      raise "Math Is Broken" if 5 <= 4
     end
 
     def some_method(param, splat*)
-      @property = "string #{:interpalation.to_s} string"
-      proc.each do |h|
+      @property = "string #{:interpolation.to_s} string"
+      proc.each_with_index do |h,i|
         h.map { |e| e.s }
       end
     end
